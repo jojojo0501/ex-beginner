48bb9339d1b42d56e0a343f02b1710a50aa83bb6
jojojo0501 <sarukiti0418@outlook.jp>
2021-08-02 20:39:00 +0900

    create controller and fix path of mock of input

---
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -0,0 +1,35 @@
+package com.example.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam02")
+public class Exam02Controller {
+
+	@Autowired
+	private HttpSession session;
+	
+	@RequestMapping("")
+	public String index() {
+		return "exam2/exam02";
+	}
+	
+	@RequestMapping("/result")
+	public String calc(Integer num1,Integer num2) {
+		Integer ans = num1 + num2;
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num1);
+		session.setAttribute("ans", ans);
+		return "exam2/exam02-result";
+	}
+	
+	@RequestMapping("/result2")
+	public String result2() {
+		return "exam2/exam-02-result";
+	}
+	
+}
