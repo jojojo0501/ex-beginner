a54afd83e553215819e3b842c5311592c3559b3a
jojojo0501 <sarukiti0418@outlook.jp>
2021-08-03 23:15:44 +0900

    create repository

---
+++ src/main/java/com/example/repository/MemberRepository.java
@@ -0,0 +1,36 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Member;
+
+@Repository
+public class MemberRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Member>MEMBER_ROW_MAPPER 
+	= (rs,i)->{
+		Member member = new Member();
+		member.setId(rs.getInt("id"));
+		member.setName(rs.getString("name"));
+		member.setAge(rs.getInt("age"));
+		member.setDep_id(rs.getInt("dep_id"));
+		return member;
+	};
+	
+	public List<Member> findByName(String name){
+		String sql = "SELECT name FROM Member WHERE name =:name;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("name", "%"+name+"%");
+		List<Member> memberList = template.query(sql, param,MEMBER_ROW_MAPPER);
+		return memberList;		
+	}
+}
